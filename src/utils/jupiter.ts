f6bff210defe989c365235b9145d8a8d71ce89b7
creeping-vampires <creepingvampires.in@gmail.com>
2024-12-23 11:13:45 +0530

    fix build issue

--- src/utils/jupiter.ts
+++ src/utils/jupiter.ts
@@ -1,4 +1,3 @@
-import { Wallet } from "@dynamic-labs/sdk-react-core";
 import { createJupiterApiClient, QuoteResponse } from "@jup-ag/api";
 
 // export async function initJupiter(connection: Connection) {
@@ -59,63 +58,3 @@ export async function getSwapObj(account: string, quote: QuoteResponse) {
   });
   return swapObj;
 }
-
-// export async function getQuoteAndSwap(wallet) {
-// //   const wallet = new Wallet(
-// //     Keypair.fromSecretKey(bs58.decode(process.env.PRIVATE_KEY || ""))
-// //   );
-// //   console.log("Wallet:", wallet.publicKey.toBase58());
-
-//   const quote = await getQuote();
-//   console.dir(quote, { depth: null });
-//   const swapObj = await getSwapObj(wallet, quote);
-//   console.dir(swapObj, { depth: null });
-
-//   // Serialize the transaction
-//   const swapTransactionBuf = Buffer.from(swapObj.swapTransaction, "base64");
-//   var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-
-//   // Sign the transaction
-//   transaction.sign([wallet.payer]);
-//   const signature = getSignature(transaction);
-
-//   // We first simulate whether the transaction would be successful
-//   const { value: simulatedTransactionResponse } =
-//     await connection.simulateTransaction(transaction, {
-//       replaceRecentBlockhash: true,
-//       commitment: "processed",
-//     });
-//   const { err, logs } = simulatedTransactionResponse;
-
-//   if (err) {
-//     // Simulation error, we can check the logs for more details
-//     // If you are getting an invalid account error, make sure that you have the input mint account to actually swap from.
-//     console.error("Simulation Error:");
-//     console.error({ err, logs });
-//     return;
-//   }
-
-//   const serializedTransaction = Buffer.from(transaction.serialize());
-//   const blockhash = transaction.message.recentBlockhash;
-
-//   const transactionResponse = await transactionSenderAndConfirmationWaiter({
-//     connection,
-//     serializedTransaction,
-//     blockhashWithExpiryBlockHeight: {
-//       blockhash,
-//       lastValidBlockHeight: swapObj.lastValidBlockHeight,
-//     },
-//   });
-
-//   // If we are not getting a response back, the transaction has not confirmed.
-//   if (!transactionResponse) {
-//     console.error("Transaction not confirmed");
-//     return;
-//   }
-
-//   if (transactionResponse.meta?.err) {
-//     console.error(transactionResponse.meta?.err);
-//   }
-
-//   console.log(`https://solscan.io/tx/${signature}`);
-// }
